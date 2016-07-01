fc2c1815927066ad807ceabea403928292f98bd3
David Bittner <bittneradave@gmail.com>
2016-07-01 11:21:43 -0400

    Forgot files

---
+++ UniqueMaster.java
@@ -0,0 +1,59 @@
+import java.util.*;
+
+public class UniqueMaster
+{
+
+    private static ArrayList<Unique> uniques = new ArrayList<>();
+
+    public static void giveKey( String key )
+    {
+
+        boolean exists = false;
+        Unique found = null;
+        for( Unique i : uniques )
+        {
+
+            if( i.ID.equals( key ) )
+            {
+
+                exists = true;
+                found = i;
+    
+            }
+
+        }
+
+        if( !exists )
+        {
+
+            uniques.add( new Unique( key ) );
+
+        }else
+        {
+
+            found.ocs++;
+
+        }
+
+    }
+
+    public static int getOccurences( String key )
+    {
+
+        for( Unique i : uniques )
+        {
+
+            if( i.ID.equals( key ) )
+            {
+
+                return i.ocs;
+
+            }
+
+        }
+
+        return 0;
+
+    }
+
+}
